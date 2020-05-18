c666a9788053ef09c1dcc34a9139a2a45e19f25c
Tyler Christian <tylerc71197@gmail.com>
2020-05-18 16:10:46 -0400

    fixed small bug i missed causing test in search to fail

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,22 +1,17 @@
 def linear_search(arr, target):
-    # Your code here
     for i in range(len(arr)):
         if arr[i] == target:
             return i
-        return i
-
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
     left = 0
     right = len(arr) - 1
 
     while left <= right:
-        mid = {left + right} // 2
+        mid = (left + right) // 2
 
         if arr[mid] == target:
             return mid
@@ -26,5 +21,4 @@ def binary_search(arr, target):
 
         else:
             right = mid - 1
-
     return -1  # not found
